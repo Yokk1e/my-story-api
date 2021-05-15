8358af54ebbf6070009e029ab5b3619aa5168966
satthabut <satthabut.o@gmail.com>
2021-05-16 02:10:45 +0700

    - [feat] update index js

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extends: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTTION_URL =
+  "mongodb+srv://yokk1e:yokk1e1234@cluster0.njc5w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
