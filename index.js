3fed7b9e05350c371a6fce26255d3b0ffb5d0134
satthabut <satthabut.o@gmail.com>
2021-05-16 03:40:25 +0700

    - [feat] add post service

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import postRouter from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRouter);
 
 app.use(bodyParser.json({ limit: "30mb", extends: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
