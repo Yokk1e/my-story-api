6b4d3e3c37768ac850f056eb42865ef63ee64778
satthabut <satthabut.o@gmail.com>
2021-05-16 07:53:00 +0700

    - [fix] cors

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ import postRouter from "./routes/posts.js";
 const app = express();
 dotenv.config();
 
+app.use(bodyParser.json({ limit: "30mb", extends: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
 app.use("/posts", postRouter);
 
 app.get('/', (req,res)=>{
     res.send('Hello to My Story API')
 })
-
-app.use(bodyParser.json({ limit: "30mb", extends: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
